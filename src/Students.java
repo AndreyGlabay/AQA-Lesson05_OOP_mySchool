afe76ab30a9857786ba9c47441b6d83d59c76297
Andriy Glabay <rishelevsky@gmail.com>
2023-11-28 23:03:17 +0200

    Add java class 'Students' and begining encapsulate some objects to it (line 1-34

---
+++ src/Students.java
@@ -0,0 +1,38 @@
+public class Students { // create new super-class
+    private final int studentId;
+    private final String studentFirstName;
+    private final String studentLastName;
+    private boolean previouslyFinishedThisSchool;
+    private short qtyOfPreviousCourses;
+    private int discount;
+
+    public Students(
+            int studentId,
+            String studentFirstName,
+            String studentLastName,
+            boolean previouslyFinishedThisSchool,
+            short qtyOfPreviousCourses,
+            int discount) {
+        this.studentId = studentId;
+        this.studentFirstName = studentFirstName;
+        this.studentLastName = studentLastName;
+        setPreviouslyFinishedThisSchool(previouslyFinishedThisSchool);
+        setQtyOfPreviousCourses(qtyOfPreviousCourses);
+        setDiscount(discount);
+    }
+
+    public int getStudentId() {return studentId;}
+
+    public String getStudentFirstName() {return studentFirstName;}
+
+    public String getStudentLastName() {return studentLastName;}
+
+    public boolean isPreviouslyFinishedThisSchool() {return previouslyFinishedThisSchool;}
+
+    public short getQtyOfPreviousCourses() {return qtyOfPreviousCourses;}
+
+    public int getDiscount() {return discount;}
+
+
+
+}
