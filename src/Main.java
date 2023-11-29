0e45673cf5483d9ab589696033f044533ba47d89
Andriy Glabay <rishelevsky@gmail.com>
2023-11-30 01:41:14 +0200

    Main class: (1) add breakout divisioners for printout; (2) add the 'DISCOUNT PROGRAM' title for print out (line 4)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
+        System.out.println("DISCOUNT PROGRAM:");
         PM id1100_001 = new PM(1, "Anna", true, 10);
         PM id1100_002 = new PM(2, "Brook", false, 0);
         PM id1100_003 = new PM(3, "Claus", false, 0);
@@ -12,12 +14,16 @@ public class Main {
         QA id1400_512 = new QA(512, "Lianne", false, 0);
         QA id1400_571 = new AQA_Java(571, "Michael", false, 0);
 
+        System.out.println();
         System.out.println("Student id1400_511 (" + id1400_511.getStudentName() + ") like to know which courses are available in their QA-Field?");
         id1400_511.message();
+        System.out.println();
         System.out.println("Student id1100_002 (" + id1100_002.getStudentName() + ") like to know which courses are available in their QA-Field?");
         id1100_002.message();
+        System.out.println();
         System.out.println("Student id1300_338 (" + id1300_338.getStudentName() + ") like to know which courses are available in their QA-Field?");
         id1300_338.message();
+        System.out.println();
 
 //        System.out.println("Info about person id1400_512");
 //        System.out.println(id1400_512 instanceof Students);
