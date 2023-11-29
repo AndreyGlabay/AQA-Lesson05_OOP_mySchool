172f050fe3d6450c9d4202d3561780765cf61810
Andriy Glabay <rishelevsky@gmail.com>
2023-11-29 20:06:08 +0200

    Modify class 'QA'

--- src/QA.java
+++ src/QA.java
@@ -1,2 +1,18 @@
 public class QA extends Students {
+    String course;
+    public Dev(
+            int studentId,
+            String studentName,
+            boolean previouslyFinishedThisSchool,
+            int discount)
+    {super(studentId, studentName, previouslyFinishedThisSchool, discount)}
+
+    @Override
+    void message(){
+        System.out.println("There are available Courses: ");
+        System.out.println("   1400.QA Engineer Manual;");
+        System.out.println("   1401.AQA Engineer (Java);");
+        System.out.println("   1402.AQA Engineer (JavaScript);");
+        System.out.println("   1403.AQA Engineer (Python);");
+    }
 }
