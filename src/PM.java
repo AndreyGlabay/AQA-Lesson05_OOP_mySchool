59aabe49b9ddced448e10051cdc49028d86187bb
Andriy Glabay <rishelevsky@gmail.com>
2023-11-29 19:39:29 +0200

    Modify class 'PM'

--- src/PM.java
+++ src/PM.java
@@ -1,2 +1,12 @@
 public class PM extends Students {
+    String course;
+    public PM(
+            int studentId,
+            String studentName,
+            boolean previouslyFinishedThisSchool,
+            int discount)
+    {super(studentId, studentName, previouslyFinishedThisSchool, discount)}
+
+    @Override
+    void message(){System.out.println("There are available Courses: 1100.Project Manager");}
 }
