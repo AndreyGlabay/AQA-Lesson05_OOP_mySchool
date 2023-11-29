ca3419791df215dde7119fbadf5ab0a67b2e092e
Andriy Glabay <rishelevsky@gmail.com>
2023-11-29 20:03:27 +0200

    Modify class 'UIUX'

--- src/UIUX.java
+++ src/UIUX.java
@@ -1,2 +1,12 @@
 public class UIUX extends Students {
+    String course;
+    public UIUX(
+            int studentId,
+            String studentName,
+            boolean previouslyFinishedThisSchool,
+            int discount)
+    {super(studentId, studentName, previouslyFinishedThisSchool, discount)}
+
+    @Override
+    void message(){System.out.println("There are available Courses: 1300.UI/UX designer");}
 }
