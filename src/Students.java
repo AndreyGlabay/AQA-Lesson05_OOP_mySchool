4f6d4f59e9c373368927a5ffbf2bb49a4c7fe8e7
Andriy Glabay <rishelevsky@gmail.com>
2023-11-30 01:21:24 +0200

    Add 'studentName' for printout at setter 'setPreviouslyFinishedThisSchool' (line 30, 33)

--- src/Students.java
+++ src/Students.java
@@ -27,10 +27,10 @@ public abstract class Students { // create new super-class
 public void setPreviouslyFinishedThisSchool(boolean previouslyFinishedThisSchool) {
         this.previouslyFinishedThisSchool = previouslyFinishedThisSchool;
         if (previouslyFinishedThisSchool) {
-            System.out.println("You have a 10% discount for the chosen Course!");
+            System.out.println(studentName + " :: " + "You have a 10% discount for the chosen Course!");
             setDiscount(10);
         } else {
-           System.out.println("Unfortunately you don't have discount for the your Course");
+           System.out.println(studentName + " :: " + "Unfortunately you don't have a discount for your Course");
            setDiscount(0);
         }
 }
