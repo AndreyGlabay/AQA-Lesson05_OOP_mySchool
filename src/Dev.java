3985c661dbe931c880f63733c0c1ab1339a7b718
Andriy Glabay <rishelevsky@gmail.com>
2023-11-29 19:57:34 +0200

    Modify class 'Dev'

--- src/Dev.java
+++ src/Dev.java
@@ -1,2 +1,17 @@
 public class Dev extends Students {
+    String course;
+    public PM(
+            int studentId,
+            String studentName,
+            boolean previouslyFinishedThisSchool,
+            int discount)
+    {super(studentId, studentName, previouslyFinishedThisSchool, discount)}
+
+    @Override
+    void message(){
+        System.out.println("There are available Courses: ");
+        System.out.println("   1201.Software Developer (Java);");
+        System.out.println("   1202.Software Developer (JavaScript);");
+        System.out.println("   1203.Software Developer (Python);");
+    }
 }
