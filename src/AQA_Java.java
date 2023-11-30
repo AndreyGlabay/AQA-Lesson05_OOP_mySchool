b63081f97eabb8572d548e2d095fd51dea7ea8b0
Andriy Glabay <rishelevsky@gmail.com>
2023-11-30 13:49:59 +0200

    Implement two field in the class AQA_Java: 'hasComExperienceInTheField' and 'yearsOfComExperience'

--- src/AQA_Java.java
+++ src/AQA_Java.java
@@ -1,5 +1,11 @@
 public class AQA_Java extends QA {
+
+    boolean hasComExperienceInTheField;
+    int yearsOfComExperience;
+
     public AQA_Java(int studentId, String studentName, String studentPhone, String studentMail, boolean previouslyFinishedThisSchool, int discount, String fieldQA) {
         super(studentId, studentName, studentPhone, studentMail, previouslyFinishedThisSchool, discount, fieldQA);
+        this.hasComExperienceInTheField = hasComExperienceInTheField;
+        this.yearsOfComExperience = yearsOfComExperience;
     }
 }
