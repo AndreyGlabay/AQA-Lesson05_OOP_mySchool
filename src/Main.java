91f5411a546eb71cbbd06ddb7b9dbc42f971ed57
Andriy Glabay <rishelevsky@gmail.com>
2023-12-28 15:23:13 +0200

    Class Main: remove extra blank lines.

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-
     private static final String FILE_NAME = "resources/students.csv";
 
     static String[] readFileUsingScanner(String fileName){
@@ -38,7 +37,7 @@ public class Main {
 
         scanner.close();
         return data.toArray(new String[0]);
-    };
+    }
 
     public static void main(String[] args) throws IOException {
         // (step 2.1) implements "List" interface (which is subclass for Collections) for ->
@@ -46,15 +45,15 @@ public class Main {
         List<Student> students = new LinkedList<>();
         String[] data = readFileUsingScanner(FILE_NAME);    // (step 2.1) read lines from the file
         createStudentObjects(data, students);               // (step 2.1) read objects from the lines
-        Stream<Student> studentStream = students.stream();         // (step 2.1) create Stream from the objects
+        Stream<Student> studentStream = students.stream();  // (step 2.1) create Stream from the objects
 
         System.out.println();
         System.out.println("****************************************************************************************");
         System.out.println("TASK 2.1 - LinkedList implementation");
         System.out.println();
 
-        System.out.println("STUDENTS: " + students); // (step 2.1) Check LinkedList: printout students' objects
-        System.out.println("TOTAL: " + students.size()); // (step 2.1) Check LinkedList: printout students' qty
+        System.out.println("STUDENTS: " + students);        // (step 2.1) Check LinkedList: printout students' objects
+        System.out.println("TOTAL: " + students.size());    // (step 2.1) Check LinkedList: printout students' qty
 
         System.out.println();
         System.out.println("****************************************************************************************");
@@ -62,31 +61,22 @@ public class Main {
         System.out.println();
 
         System.out.println("Elements in the Stream :");             // (step 2.2) check Stream functionality:
+        System.out.println();
         Stream<String> lines = Files.lines(Paths.get(FILE_NAME));   // (step 2.2) get string lines from the file,
         lines.forEach(System.out::println);                         // (step 2.2) printout each line
-
-
-
 
         System.out.println();
         System.out.println("****************************************************************************************");
         System.out.println("TASK 2.3 - One general Stack implementation");
         System.out.println();
 
-
-
         Stack<Student> studentStack = new Stack<>();        // (step 2.3) modify List to the Stack of all students
         studentStack.addAll(students);                      // (step 2.3) add all elements from the List to the Stack;
         System.out.println("All elements in the 1 Stack :");// (step 2.3) check Stack functionality:
+        System.out.println();
         while (!studentStack.isEmpty()) {                   // (step 2.3) until Stack is not empty ->
             System.out.println(studentStack.pop());         // (step 2.3) -> take of an element from the Stack and return it;
         }
-
-
-
-
-
-
 
         System.out.println();
         System.out.println("****************************************************************************************");
@@ -112,12 +102,11 @@ public class Main {
 
         System.out.println("Elements divided to the 4 Stacks:");
         System.out.println();
+
         System.out.println("PM Stack: " + pmStack);         // (step 2.4) checkout pmStack;
         System.out.println("Dev Stack: " + devStack);       // (step 2.4) checkout devStack;
         System.out.println("UIUX Stack: " + uiuxStack);     // (step 2.4) checkout uiuxStack;
         System.out.println("QA Stack: " + qaStack);         // (step 2.4) checkout qaStack;
-
-
 
         System.out.println();
         System.out.println("****************************************************************************************");
@@ -129,22 +118,16 @@ public class Main {
         Stream<Student> uiuxStream = uiuxStack.stream();    // (step 3) implement Stream for UIUX Stack
         Stream<Student> qaStream = qaStack.stream();        // (step 3) implement Stream for QA Stack
 
-
-
         System.out.println("TASK 3.1 - Streams' Sorting");
         System.out.println();
         System.out.println("PM Students Stream Sorting: ");
         System.out.println();
-
-
 
         pmStream        // (step 3.1) for PM Stream for "studentId" field implement:
                 .sorted((s1, s2) -> (int) (s1.getStudentId() - s2.getStudentId()))      // sorting with SortOrder = ASC,
                 .skip(7)                                                             // SortSkip = 7 -> skip id [11;17],
                 .limit(13)                                                      // SortOut = 13 -> show id [18;30],
                 .forEach(System.out::println);                                          // printout the Stream;
-
-
 
         System.out.println();
         System.out.println("****************************************************************************************");
@@ -158,7 +141,6 @@ public class Main {
                 .skip(20)                                           // choose any "start point" (skip first 20 id)
                 .limit(24)                                     // FilterOut = 24 -> show 24 id
                 .forEach(System.out::println);                         // printout the Stream;
-
 
         System.out.println();
         System.out.println("****************************************************************************************");
@@ -174,7 +156,6 @@ public class Main {
         ));
         devMap.forEach((key, value) -> System.out.println("ID = " + key + " -> Name = " + value));
 
-
         System.out.println();
         System.out.println("Map for UI/UX Students: ");
         System.out.println();
@@ -186,7 +167,6 @@ public class Main {
         ));
         uiuxMap.forEach((key, value) -> System.out.println("ID = " + key + " -> Call: " + value));
 
-
         System.out.println();
         System.out.println("Map for QA Students: ");
         System.out.println();
@@ -197,11 +177,6 @@ public class Main {
                 student -> student.getStudentName() + " on address: " + student.getStudentMail()
         ));
         qaMap.forEach((key, value) -> System.out.println("ID = " + key + " -> Mail to: " + value));
-
-
-
-
-
     }
 
     private  static void createStudentObjects(String[] data, List<Student> students) {
@@ -233,24 +208,6 @@ public class Main {
             }
 
             students.add(student); // (step 2.1) implement method "add()", which add data for Linked List
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
