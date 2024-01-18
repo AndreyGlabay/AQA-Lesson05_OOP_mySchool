386122964b741a114c08733c3eb0a359f198acf5
Andriy Glabay <rishelevsky@gmail.com>
2024-01-18 18:53:10 +0200

    Test-classes: 'ObjectDataTests' and 'PriorityTests': implement 5 test methods in each test-class.

--- src/test/java/com/example/school/tests/ObjectDataTests.java
+++ src/test/java/com/example/school/tests/ObjectDataTests.java
@@ -1,4 +1,65 @@
-package com.example.school.tests;
+package com.example.school.tests; // (step 1) Auto-created, when implemented ObjectDataTests java-class;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.Objects;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class ObjectDataTests {
+    @DataProvider
+    public Object[][] data() {
+        return new Object[][] {
+                {41, "Alson", "+17505151041", "alson@probe.com"},
+                {276, "Indira", "+17505151276", "indira@probe.com"},
+                {376, "Prince", "+17505151376", "prince@probe.com"},
+                {601, "Yowl", "+17505151601", "yowl@probe.com"}
+        };
+    }
+
+    // (step 1) Implement 5 test methods with use data from the data provider;
+    @Test(dataProvider = "data")
+    public void testStudentId(int studentId, String studentName, String studentPhone, String studentMail) {
+        int expectedStudentId41 = 41;
+        var softAssert = new SoftAssert();
+        softAssert.assertEquals(studentId, expectedStudentId41);
+    }
+
+    @Test(dataProvider = "data")
+    public void testStudentName(int studentId, String studentName, String studentPhone, String studentMail) {
+        String expectedStudentNameAlson = "Alson";
+        String expectedStudentNameBert = "Bert";
+        String expectedStudentNameYowl = "Yowl";
+        String expectedStudentNameZara = "Zara";
+
+        assertTrue(
+                Objects.equals(studentName, expectedStudentNameAlson) ||
+                        Objects.equals(studentName, expectedStudentNameBert) ||
+                        Objects.equals(studentName, expectedStudentNameYowl) ||
+                        Objects.equals(studentName, expectedStudentNameZara),
+                "Actual studentName does not match any of the expected studentName.");
+    }
+
+    @Test(dataProvider = "data")
+    public void testStudentPhone(int studentId, String studentName, String studentPhone, String studentMail) {
+        String expectedStudentPhone276 = "+17505151276";
+        var softAssert = new SoftAssert();
+        softAssert.assertEquals(studentPhone, expectedStudentPhone276);
+    }
+
+    @Test(dataProvider = "data")
+    public void testStudentMail(int studentId, String studentName, String studentPhone, String studentMail) {
+        String expectedStudentMail376 = "prince@probe.com";
+        var softAssert = new SoftAssert();
+        softAssert.assertEquals(studentMail, expectedStudentMail376);
+    }
+
+    @Test(dataProvider = "data")
+    public void testStudentPhoneNotNull(int studentId, String studentName, String studentPhone, String studentMail) {
+        var softAssert = new SoftAssert();
+        softAssert.assertNotNull(studentPhone);
+    }
 }
--- src/test/java/com/example/school/tests/PriorityTests.java
+++ src/test/java/com/example/school/tests/PriorityTests.java
@@ -1,4 +1,22 @@
-package com.example.school.tests;
+package com.example.school.tests; // (step 1) Auto-created, when implemented PriorityTests java-class;
+
+import org.testng.annotations.Test;
 
 public class PriorityTests {
+    // (step 1) Implement 5 test methods with different priorities;
+    @Test(priority = 1)
+    public void test_A1() {System.out.println("Test_A1");}
+
+    @Test(priority = 2)
+    public void test_A2() {System.out.println("Test_A2");}
+
+    @Test(priority = 1)
+    public void test_B1() {System.out.println("Test_B1");}
+
+    @Test(priority = 2)
+    public void test_B2() {System.out.println("Test_B2");}
+
+    @Test(priority = 3)
+    public void test_B3() {System.out.println("Test_B3");}
+
 }
