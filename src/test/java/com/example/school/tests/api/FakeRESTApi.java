f4b27840d4c5e3ace9fb0ea11c00e965ed87b672
Andriy Glabay <rishelevsky@gmail.com>
2024-01-28 13:56:18 +0200

    Test class 'FakeRESTApi': (1) Initiate new vars: apiURL = address of the resource, endpointName = endpoint acc. to the task, url = resource + endpoint.

--- src/test/java/com/example/school/tests/api/FakeRESTApi.java
+++ src/test/java/com/example/school/tests/api/FakeRESTApi.java
@@ -1,4 +1,12 @@
-package com.example.school.tests.api; // (step 1) Create new package "api"
+package com.example.school.tests.api; // (step 1.a) Create new package "api"
 
-public class FakeRESTApi { // (step 1) Create new test class "FakeRESTApi"
+import org.testng.annotations.Test;
+
+public class FakeRESTApi { // (step 1.b) Create new test class "FakeRESTApi"
+    final String apiUrl = "https://fakerestapi.azurewebsites.net/api/v1"; // (1.c) Create var = Copy URL from the resource;
+    @Test
+    public void authors() {
+        final String endpointName = "/Authors"; // (1.d) Initiate var "endpointName" = endpoint acc. to the task;
+        String url = apiUrl + endpointName; // (1.e) Initiate var "url", which consist of api and endpoint;
+    }
 }
