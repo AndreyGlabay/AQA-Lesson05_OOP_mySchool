d8636960826c28fe558b46de115c859d0f91a0c7
Andriy Glabay <rishelevsky@gmail.com>
2024-01-28 14:29:07 +0200

    pom.xml: make new dependency from Maven repo for OkHTTP Client.

--- src/test/java/com/example/school/tests/api/FakeRESTApi.java
+++ src/test/java/com/example/school/tests/api/FakeRESTApi.java
@@ -1,12 +1,14 @@
 package com.example.school.tests.api; // (step 1.a) Create new package "api"
 
 import org.testng.annotations.Test;
+import okhttp3.OkHttpClient;
 
 public class FakeRESTApi { // (step 1.b) Create new test class "FakeRESTApi"
     final String apiUrl = "https://fakerestapi.azurewebsites.net/api/v1"; // (1.c) Create var = Copy URL from the resource;
+    OkHttpClient client = new Ok
     @Test
-    public void authors() {
-        final String endpointName = "/Authors"; // (1.d) Initiate var "endpointName" = endpoint acc. to the task;
-        String url = apiUrl + endpointName; // (1.e) Initiate var "url", which consist of api and endpoint;
+    public void authors() { // (1.d) Create test method "authors()";
+        final String endpointName = "/Authors"; // (1.e) Initiate var "endpointName" = endpoint acc. to the task;
+        String url = apiUrl + endpointName; // (1.f) Initiate var "url", which consist of api and endpoint;
     }
 }
