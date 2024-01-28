476be84f682e5ba4c17a2f5d729c131ad3a3c956
Andriy Glabay <rishelevsky@gmail.com>
2024-01-28 20:06:27 +0200

    Class Authors: implement vars according to the Authors-GET-JSON scheme.

--- src/test/java/com/example/school/tests/api/dto/Authors.java
+++ src/test/java/com/example/school/tests/api/dto/Authors.java
@@ -1,4 +1,8 @@
 package com.example.school.tests.api.dto; // (step 1.l) Create new package "dto";
 
 public class Authors { // (step 1.m) In the "dto" package: create new java-class "Authors" for make DTO for the entity;
+    int id; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
+    int idBook; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
+    String firstName; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
+    String lastName; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
 }
