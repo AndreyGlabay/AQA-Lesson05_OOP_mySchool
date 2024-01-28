2d8717799d70d010ff8b9db3a189f602f95bf7e5
Andriy Glabay <rishelevsky@gmail.com>
2024-01-28 21:38:09 +0200

    Class Authors: (1) annotate variables with '@JsonProperty'; (2) add accessors for all fields; (3) add method toString().

--- src/test/java/com/example/school/tests/api/dto/Authors.java
+++ src/test/java/com/example/school/tests/api/dto/Authors.java
@@ -1,8 +1,58 @@
 package com.example.school.tests.api.dto; // (step 1.l) Create new package "dto";
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Authors { // (step 1.m) In the "dto" package: create new java-class "Authors" for make DTO for the entity;
-    int id; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
-    int idBook; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
-    String firstName; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
-    String lastName; // (step 1.n) implement var according to the Authors - GET -JSON scheme;
+    // (step 1.n) implement variables according to the /Authors-GET-scheme;
+    // (step 1.q) annotate variables with "@JsonProperty", add accessors, add toString;
+    @JsonProperty
+    int id;
+    @JsonProperty
+    int idBook;
+    @JsonProperty
+    String firstName;
+    @JsonProperty
+    String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(int idBook) {
+        this.idBook = idBook;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Authors{" +
+                "id=" + id +
+                ", idBook=" + idBook +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
