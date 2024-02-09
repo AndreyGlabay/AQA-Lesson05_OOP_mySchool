defacf7901ef25c9ccb0d5f264eee86bd3de87c6
Andriy Glabay <rishelevsky@gmail.com>
2024-02-09 20:29:33 +0200

    Class Main: (1) Delete package java.util.* (2) Add missed packages.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
