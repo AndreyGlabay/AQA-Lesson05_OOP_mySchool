aece53a0b22f0d48aa5186189e886bf6e4229311
Andriy Glabay <rishelevsky@gmail.com>
2024-02-11 19:02:18 +0200

    Test Class AnalyticsTest: implements new OkHttpClient

--- src/test/java/com/example/school/tests/AnalyticsTest.java
+++ src/test/java/com/example/school/tests/AnalyticsTest.java
@@ -1,5 +1,7 @@
 package com.example.school.tests; // (2.1.a) In the dir src/test/java create new package "com.example.school.tests";
 
-public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests" create new test class "AnalyticsTest";
+import okhttp3.OkHttpClient;
 
+public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests" create new test class "AnalyticsTest";
+    OkHttpClient client = new OkHttpClient.Builder().build(); // (2.1.c) Implements new OkHttpClient;
 }
