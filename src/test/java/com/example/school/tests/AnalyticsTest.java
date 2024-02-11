d2f6268921baa873e7231df13dfb3c9f72a6fad2
Andriy Glabay <rishelevsky@gmail.com>
2024-02-11 19:46:43 +0200

    Annotation @BeforeClass: Implements configuration creating using port num.

--- src/test/java/com/example/school/tests/AnalyticsTest.java
+++ src/test/java/com/example/school/tests/AnalyticsTest.java
@@ -1,6 +1,7 @@
 package com.example.school.tests; // (2.1.a) In the dir src/test/java create new package "com.example.school.tests";
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import okhttp3.OkHttpClient;
 import org.testng.annotations.AfterClass;
@@ -15,6 +16,7 @@ public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests
     @BeforeClass // (2.1.e) Implements @BeforeClass annotation
     public void beforeClass(){
         wireMockServer.start(); // (2.1.f) Implements start of wireMockServer;
+        WireMock.configureFor(9098); // (2.1.g) Implements configuration creating using port num;
 
     }
     @AfterClass // (2.1.e) Implements @AfterClass annotation
