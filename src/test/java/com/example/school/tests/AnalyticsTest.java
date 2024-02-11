9af65a70e1bcca3a78bf96d9a78fc0c305176ffe
Andriy Glabay <rishelevsky@gmail.com>
2024-02-11 19:07:32 +0200

    Test Class AnalyticsTest: Implements new WireMockServer;

--- src/test/java/com/example/school/tests/AnalyticsTest.java
+++ src/test/java/com/example/school/tests/AnalyticsTest.java
@@ -1,7 +1,11 @@
 package com.example.school.tests; // (2.1.a) In the dir src/test/java create new package "com.example.school.tests";
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import okhttp3.OkHttpClient;
 
 public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests" create new test class "AnalyticsTest";
     OkHttpClient client = new OkHttpClient.Builder().build(); // (2.1.c) Implements new OkHttpClient;
+    WireMockServer wireMockServer
+            = new WireMockServer(new WireMockConfiguration().port(9098));//(2.1.d)Implements new WireMockServer;
 }
