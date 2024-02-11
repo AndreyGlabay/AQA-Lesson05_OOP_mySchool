76c6ef72ebaedb68d295e856f955f63c30f57e46
Andriy Glabay <rishelevsky@gmail.com>
2024-02-11 19:49:52 +0200

    Test Class AnalyticsTest: implements and initiate var 'port'.

--- src/test/java/com/example/school/tests/AnalyticsTest.java
+++ src/test/java/com/example/school/tests/AnalyticsTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests" create new test class "AnalyticsTest";
+    int port = 9098; // (2.1.g) Implements var "port" and initiate it;
     OkHttpClient client = new OkHttpClient.Builder().build(); // (2.1.c) Implements new OkHttpClient;
     WireMockServer wireMockServer
             = new WireMockServer(new WireMockConfiguration().port(9098));//(2.1.d)Implements new WireMockServer;
@@ -16,7 +17,7 @@ public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests
     @BeforeClass // (2.1.e) Implements @BeforeClass annotation
     public void beforeClass(){
         wireMockServer.start(); // (2.1.f) Implements start of wireMockServer;
-        WireMock.configureFor(9098); // (2.1.g) Implements configuration creating using port num;
+        WireMock.configureFor(port); // (2.1.g) Implements configuration creating using port num;
 
     }
     @AfterClass // (2.1.e) Implements @AfterClass annotation
