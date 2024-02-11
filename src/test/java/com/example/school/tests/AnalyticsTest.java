acf9b71fc4c6baa6c3a2697a639b4dc3fe3dfba7
Andriy Glabay <rishelevsky@gmail.com>
2024-02-11 19:36:00 +0200

    Test Class AnalyticsTest: Implements BeforeClass and AfterClass annotations.

--- src/test/java/com/example/school/tests/AnalyticsTest.java
+++ src/test/java/com/example/school/tests/AnalyticsTest.java
@@ -3,9 +3,21 @@ package com.example.school.tests; // (2.1.a) In the dir src/test/java create new
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import okhttp3.OkHttpClient;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests" create new test class "AnalyticsTest";
     OkHttpClient client = new OkHttpClient.Builder().build(); // (2.1.c) Implements new OkHttpClient;
     WireMockServer wireMockServer
             = new WireMockServer(new WireMockConfiguration().port(9098));//(2.1.d)Implements new WireMockServer;
+
+
+    @BeforeClass // (2.1.e) Implements BeforeClass annotation
+    public void beforeClass(){
+
+    }
+    @AfterClass // (2.1.e) Implements AfterClass annotation
+    public void afterClass(){
+
+    }
 }
