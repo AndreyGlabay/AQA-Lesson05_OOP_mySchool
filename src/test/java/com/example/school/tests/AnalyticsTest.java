25548bd12d8da540ceaa7916f11a96143b4e326a
Andriy Glabay <rishelevsky@gmail.com>
2024-02-11 18:57:47 +0200

    Package com.example.school.tests: create new test class 'AnalyticsTest'.

---
+++ src/test/java/com/example/school/tests/AnalyticsTest.java
@@ -0,0 +1,5 @@
+package com.example.school.tests; // (2.1.a) In the dir src/test/java create new package "com.example.school.tests";
+
+public class AnalyticsTest { // (2.1.b) In the package "com.example.school.tests" create new test class "AnalyticsTest";
+
+}
